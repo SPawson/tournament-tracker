1e48ba9e4ed3aed1fe9bfae82a9569939bca7f5f
Sam Pawson <samuel.pawson@gmail.com>
2021-06-16 21:43:38 +0100

    initial commit

---
+++ TournamentTracker/TrackerLibrary/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary.Models
+{
+   public class PersonModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ TournamentTracker/TrackerLibrary/Models/TeamModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary.Models
+{
+    public class TeamModel
+    {
+        public string Name { get; set; }
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+    }
+}
