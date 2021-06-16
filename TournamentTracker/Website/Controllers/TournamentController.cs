13b869b83db5b9ed9e8870668d35a2423aa1b19c
Sam Pawson <samuel.pawson@gmail.com>
2021-06-16 23:11:16 +0100

    added create tournament basic view

---
+++ TournamentTracker/Website/Controllers/TournamentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class TournamentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("~/Views/Tournament/CreateTournament.cshtml");
+        }
+    }
+}
